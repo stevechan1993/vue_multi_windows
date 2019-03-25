4549d4f5f880e33833692679c665c2caa7b6c077
stevechan1993 <1132531766@qq.com>
2019-03-25 17:46:50 +0800

    测试指令

--- server/framework/tlv/decrypt.js
+++ server/framework/tlv/decrypt.js
@@ -12,5 +12,5 @@ function decrypt(data) {
 // console.log(decrypt('4e66001721037b22726f626f74223a312c22737461747573223a307d3f6e5dbc'))
 module.exports = {decrypt};
 
-const b = decrypt('4e66000e21037b2274696d65223a31323133347d9efb02e7');
-console.log(b);
+// const b = decrypt('4e66000e21037b2274696d65223a31323133347d9efb02e7');
+// console.log(b);
